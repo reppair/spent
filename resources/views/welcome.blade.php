373fb964dc025fccad680034855a207c08d1a861
Martin Blagoev <mb.reppair@gmail.com>
2025-12-30 18:51:56 +0200

    setup PWA requirements

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,17 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{ config('app.name') }}</title>
+        <meta name="description" content="Track what you spend.">
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        <!-- Icons -->
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" href="/favicon-32x32.png" type="image/png" sizes="32x32">
+        <link rel="icon" href="/favicon-16x16.png" type="image/png" sizes="16x16">
+        <link rel="manifest" href="/build/manifest.webmanifest">
+        <meta name="theme-color" content="#FFFFFF" media="(prefers-color-scheme: light)">
+        <meta name="theme-color" content="#000000" media="(prefers-color-scheme: dark)">
+
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import {
 } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
     plugins: [
@@ -11,6 +12,45 @@ export default defineConfig({
             refresh: true,
         }),
         tailwindcss(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                "name": "Spent",
+                "short_name": "Spent",
+                "Description": "Track what you spend.",
+                "icons": [
+                    {
+                        "src": "/pwa-192x192.png",
+                        "sizes": "192x192",
+                        "type": "image/png",
+                        "purpose": "any"
+                    },
+                    {
+                        "src": "/pwa-512x512.png",
+                        "sizes": "512x512",
+                        "type": "image/png",
+                        "purpose": "any"
+                    },
+                    {
+                        "src": "/pwa-maskable-192x192.png",
+                        "sizes": "192x192",
+                        "type": "image/png",
+                        "purpose": "any maskable"
+                    },
+                    {
+                        "src": "/pwa-maskable-512x512.png",
+                        "sizes": "512x512",
+                        "type": "image/png",
+                        "purpose": "any maskable"
+                    }
+                ],
+                "start_url": "/",
+                "scope": "/",
+                "display": "standalone",
+                "background_color": "#FFFFFF",
+                "theme_color": "#FFFFFF"
+            }
+        }),
     ],
     server: {
         cors: true,
