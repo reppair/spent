50b9ae0309888b137690950daeeeefee5e00f82f
Martin Blagoev <mb.reppair@gmail.com>
2025-12-31 20:11:47 +0200

    adjust flux header theming in app layout

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,8 +4,8 @@
         @include('partials.head')
     </head>
 
-    <body class="min-h-screen bg-white dark:bg-zinc-800">
-        <flux:header container class="border-b border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
+    <body class="min-h-screen bg-white dark:bg-zinc-900">
+        <flux:header container class="border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
             <a href="{{ route('dashboard') }}" class="ms-2 me-5 flex items-center space-x-2 rtl:space-x-reverse lg:ms-0" wire:navigate>
                 <x-app-logo />
             </a>
