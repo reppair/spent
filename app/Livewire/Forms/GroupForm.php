a4fbc80d18c89ee8491288d42e42d4270b2f1f86
Martin Blagoev <mb.reppair@gmail.com>
2026-01-12 08:42:54 +0200

    add comment

--- app/Livewire/Forms/GroupForm.php
+++ app/Livewire/Forms/GroupForm.php
@@ -7,6 +7,7 @@ use Livewire\Form;
 
 class GroupForm extends Form
 {
+    // we don't care about groups with same name per user whatsoever
     #[Validate('required|string|max:255')]
     public string $name = '';
 }
